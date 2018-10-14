a6899b51a405ea7dc309e720c0eaaa8517ee33b2
Ajay <ajaymungur@hotmail.com>
2018-10-14 15:59:39 +0000

    commit 8 - added user inputfunctionality

--- directory.rb
+++ directory.rb
@@ -1,33 +1,40 @@
 # lets put all the students into an array
-students = [
-    {name: "Dr. Hannibal Lecter", cohort: :november},
-    {name: "Darth Vader", cohort: :november},
-    {name: "Nurse Ratched", cohort: :november},
-    {name: "Michael Corleone", cohort: :november},
-    {name: "Alex DeLarge", cohort: :november},
-    {name: "The Wicked Witch of the West", cohort: :november},
-    {name: "Terminator", cohort: :november},
-    {name: "Freddy Krueger", cohort: :november},
-    {name: "The Joker", cohort: :november},
-    {name: "Joffrey Baratheon", cohort: :november},
-    {name: "Norman Bates", cohort: :november}
-]
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp.capitalize
+  # while name is not empty repeat this code
+  while !name.empty?
+    # add the student to the hash array
+    students << {name: name, cohort: :november}
+    puts "We now have #{students.count} students"
+    # get another name from the user
+    name = gets.chomp.capitalize
+  end
+  # return the array of students
+  students
+end
 
 # and then print them
 def print_header
-puts "The students of Villain Academy"
-puts "-------------------------------"
+  puts "The students of Villain Academy"
+  puts "-------------------------------"
 end 
 
 def print(names)
-names.each { |names| puts "#{names[:name]} (#{names[:cohort]} cohort)" }
+  names.each { |names| puts "#{names[:name]} (#{names[:cohort]} cohort)" }
 end
 
 def print_footer(names)
-puts "Overall, we have #{names.count} great students"
+  puts "Overall, we have #{names.count} great students"
 end
 
 # nothing happens until we call the methods
+students = input_students
 print_header
 print(students)
 print_footer(students)
