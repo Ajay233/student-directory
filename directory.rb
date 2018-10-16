52f79cce18b4edbb0ee4a1bf632e54740751139a
Ajay <ajaymungur@hotmail.com>
2018-10-16 19:53:57 +0000

    commit 10 - Only prints names beginning with specific letter

--- directory.rb
+++ directory.rb
@@ -27,7 +27,12 @@ end
 
 def print(names)
   # Now prints a number before the name of each student
-  names.each_with_index { |names, index| puts "#{index + 1}.#{names[:name]} (#{names[:cohort]} cohort)" }
+  names.each_with_index do |names, index|  
+    # Now only prints names beginning with "D"
+    if names[:name].starts_with?("D")
+      puts "#{index + 1}.#{names[:name]} (#{names[:cohort]} cohort)"
+    end
+  end
 end
 
 def print_footer(names)
