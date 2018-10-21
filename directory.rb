a5d59e3aefafc07d5f3cc143a387173abb99223b
Ajay <ajaymungur@hotmail.com>
2018-10-21 16:50:37 +0000

    commit 21 - Refactored interactive menu method and added global vairable

--- directory.rb
+++ directory.rb
@@ -1,3 +1,5 @@
+@students = []
+
 # This method makes sure either a valid month or blank string is entered
 def cohort_validation(month)
   checks = ["January", "February","March", "April", "May", "June",
@@ -28,8 +30,6 @@ end
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create an empty array
-  students = []
   # get the first name
   name = gets.delete!("\n").capitalize
   # while name is not empty repeat this code
@@ -54,22 +54,20 @@ def input_students
     hobbies = gets.delete!("\n").capitalize
     
     # add the student details to the hash array
-    students << {name: name, age: age, height: height, home_town: town,
+    @students << {name: name, age: age, height: height, home_town: town,
                  country_of_birth: country, gov_dept: dept,
                  requirements: requirements, hobbies: hobbies, cohort: cohort}
     # catches any empty values and assigns "TBC" as the default value
-    default(students)
+    default(@students)
     # Ensures the sentence reads correctly with student count
-    if students.count == 1
-      puts "We now have #{students.count} student"
+    if @students.count == 1
+      puts "We now have #{@students.count} student"
     else
-      puts "We now have #{students.count} students"
+      puts "We now have #{@students.count} students"
     end
     # get another name from the user
     name = gets.delete!("\n").capitalize
   end
-  # return the array of students
-  students
 end
 
 # and then print them
@@ -78,10 +76,10 @@ def print_header
   puts "-------------------------------".center(160)
 end
 
-def print(names)
+def print_students
   # Create a list of all cohorts that have been entered
   cohorts = []
-  names.each { |hash| cohorts << hash[:cohort] }
+  @students.each { |hash| cohorts << hash[:cohort] }
   cohorts.uniq!
   x = 0
   # Iterates through each cohort month in the cohorts array
@@ -89,18 +87,18 @@ def print(names)
     puts "The #{cohorts[x]} cohort\n".center(160)
     i = 0
     # "Each" changed to an until loop
-    until i >= names.length
+    until i >= @students.length
     # Now prints a number before the name of each student
       # Now only prints names beginning with "D" & containing less than 12 letters
-      if names[i][:name].start_with?("D") && names[i][:name].length < 12 && cohorts[x] == names[i][:cohort]
-        puts "#{i + 1}.Name: #{names[i][:name]} (#{names[i][:cohort]} cohort)".center(160)
-        puts "Age: #{names[i][:age]}".center(160)
-        puts "Height: #{names[i][:height]} m".center(160)
-        puts "Home Town: #{names[i][:home_town]}".center(160)
-        puts "Birth Country: #{names[i][:country_of_birth]}".center(160)
-        puts "Government Department: #{names[i][:gov_dept]}".center(160)
-        puts "Special Requirements: #{names[i][:requirements]}".center(160)
-        puts "Hobbies: #{names[i][:hobbies]}".center(160)
+      if @students[i][:name].start_with?("D") && @students[i][:name].length < 12 && cohorts[x] == @students[i][:cohort]
+        puts "#{i + 1}.Name: #{@students[i][:name]} (#{@students[i][:cohort]} cohort)".center(160)
+        puts "Age: #{@students[i][:age]}".center(160)
+        puts "Height: #{@students[i][:height]} m".center(160)
+        puts "Home Town: #{@students[i][:home_town]}".center(160)
+        puts "Birth Country: #{@students[i][:country_of_birth]}".center(160)
+        puts "Government Department: #{@students[i][:gov_dept]}".center(160)
+        puts "Special Requirements: #{@students[i][:requirements]}".center(160)
+        puts "Hobbies: #{@students[i][:hobbies]}".center(160)
         puts
       end
     i += 1  
@@ -110,29 +108,35 @@ def print(names)
   end
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students".center(160)
+def print_footer
+  puts "Overall, we have #{@students.count} great students".center(160)
+end
+
+def print_menu
+  puts "1. Input students"
+  puts "2. Show students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_students
+  print_footer
+end
+
+def process(selection)
+  case selection
+    when "1" then input_students
+    when "2" then show_students
+    when "9" then exit
+    else puts "I don't know what you meant, try again"
+  end
 end
 
 def interactive_menu
-  students = []
   loop do
-    puts "1. Input students"
-    puts "2. Show students"
-    puts "9. Exit"
-    selection = gets.chomp
-    case selection
-      when "1"
-        students = input_students
-      when "2" then
-        print_header
-        print(students)
-        print_footer(students)
-      when "9" then
-        exit
-      else
-        puts "I don't know what you meant, try again"
-    end
+    print_menu
+    process(gets.chomp)
   end
 end
 
